1f074599d1aed8ec7bf884480d2c49805293e66f
Daniel-S23 <daniel.sa142015@gmail.com>
2023-01-17 01:51:30 -0500

    proyecto agregado

---
+++ scripts/formulario.js
@@ -0,0 +1,48 @@
+const carritoCont = document.querySelector('.carritoModal')
+const btnPagar = document.getElementById('btnPagar')
+
+const pintarCarrito = () => {
+
+  Object.values(carrito).forEach(producto => {
+
+    carritoCont.innerHTML += `
+        <div class="carritoCard " id="carritoCard">
+            <img src="${producto.imagen}" alt="">
+            <div class="carritoInfo ">
+                <p>${producto.nombre}</p>
+                <p>$${Math.ceil(producto.precio * producto.cantidad)}</p>
+            </div>
+            <div class="selectorU">
+               
+                <p id="cantidad">${producto.cantidad}</p>
+
+            </div>
+        </div>
+        `
+  })
+  const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
+  btnPagar.innerHTML = `Pagar $${Math.ceil(nPrecio)}`
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+
+}
+
+if (localStorage.getItem('carrito')) {
+  carrito = JSON.parse(localStorage.getItem('carrito'))
+  pintarCarrito()
+}
+function comprado() {
+  Swal.fire(
+    'The Internet?',
+    'That thing is still around?',
+    'question'
+  )
+}
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const acumulador = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
+  Swal.fire(
+    `Compra Exitosa por $${acumulador}`,
+    'Gracias por preferirnos',
+    'success'
+  )
+})
